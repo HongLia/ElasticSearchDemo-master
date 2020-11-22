4f46040a7edb5df1754ac73b343b0f5d300732b1
HongLia <hongliang9458@163.com>
2020-11-23 00:17:54 +0800

    添加新方法

---
+++ src/main/java/com/es/controller/SearchController.java
@@ -0,0 +1,28 @@
+package com.es.controller;
+
+import com.es.EsApi.SearchApi;
+import com.es.util.Response;
+import org.elasticsearch.client.RestClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SearchController {
+    @Autowired
+    SearchApi searchApi;
+
+    @PostMapping("/searchTest")
+    @ResponseBody
+    public Response search() throws Exception {
+
+        RestClient restClient = searchApi.getRestClient();
+        searchApi.queryClusterInfo(restClient);
+        boolean big = searchApi.indexIsExist(restClient, "fas_index");
+        System.out.println("big = " + big);
+
+
+        return null;
+    }
+}
