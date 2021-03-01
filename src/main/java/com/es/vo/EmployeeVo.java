a1ad3a56bd89f855163d45862570a78d2130e36a
zhang <hongliang9458@163.com>
2021-03-01 14:27:10 +0800

    添加Lombok注解

--- src/main/java/com/es/vo/EmployeeVo.java
+++ src/main/java/com/es/vo/EmployeeVo.java
@@ -9,35 +9,5 @@ public class EmployeeVo {
     private Integer age;
     private String about;
 
-    public Long getId() {
-        return id;
-    }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    public String getAbout() {
-        return about;
-    }
-
-    public void setAbout(String about) {
-        this.about = about;
-    }
 }
